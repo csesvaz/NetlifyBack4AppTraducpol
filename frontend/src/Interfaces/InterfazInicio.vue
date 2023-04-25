758c41bad55e1f30f7bfa356871ed5efc657cf78
Csesvaz <camilocococo@gmail.com>
2023-04-25 23:53:04 +0200

    Subidas Imágenes

---
+++ frontend/src/Interfaces/InterfazInicio.vue
@@ -0,0 +1,26 @@
+<script>
+import LogoComponent from "../components/LogoComponent.vue";
+import PlaceHolders from "../components/PlaceHolders.vue";
+export default {
+  components: { LogoComponent, PlaceHolders, BarraNavegacion, FooterInicio },
+};
+</script>
+
+<template>
+  <div class="logo-container">
+  </div>
+  <main>
+    <div class="logo-container">
+      <LogoComponent />
+    </div>
+    <PlaceHolders />
+  </main>
+  <footer>
+  </footer>
+</template>
+<style scoped>
+.logo-container {
+  margin-bottom: 1em;
+}
+
+</style>
