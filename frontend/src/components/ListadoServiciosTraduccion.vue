0b345ad9c3985563d44f6f291d408e4dbc2ca5f0
Csesvaz <camilocococo@gmail.com>
2023-04-30 08:54:54 +0200

    Introducido objetor en ListadoTraduccion

--- frontend/src/components/ListadoServiciosTraduccion.vue
+++ frontend/src/components/ListadoServiciosTraduccion.vue
@@ -1,4 +1,13 @@
-<script></script>
+<script>
+import { mapState } from "pinia";
+import { useEmpresaStore } from "../stores/EmpresaStore";
+export default {
+  computed: {
+    ...mapState(useEmpresaStore, ["empresas"]),
+  },
+};
+
+</script>
 <template>
   <div class="container-fluid mt-4">
     <h3>SERVICIOS DE TRADUCCIÓN</h3>
@@ -10,28 +19,30 @@
       <span class="col-2 bg-primary border border-dark">Traducción Jurada</span>
     </div>
     <!-- v-for de los servicios de Traducción-->
-
-    <div class="row mb-12">
-      <span class="col-2 bg-ligth border border-dark">nombreEmpresa</span>
-      <span class="col-2 bg-ligth border border-dark">Idioma</span>
-      <span class="col-3 bg-ligth border border-dark"> tipoDocumento </span>
-      <span class="col-2 bg-ligth border border-dark">tiempoEntrega </span>
-      <span class="col-2 bg-ligth border border-dark"> traduccionJurada </span>
-      <!-- <span
-          class="col-1 mt-2"> -->
-      
-      <!-- <fa class="lapiz" icon="fa-solid fa-pencil" size="2xl"
-          />
-          <fa
-            class="bin"
-            icon="fa-solid fa-trash-arrow-up"
-            size="2xl"
-            style="color: #c01c28"
-          /> -->
-      <!-- </span> -->
+    <div v-for="empresa in empresas" :key="empresa.id" >
+      <div v-for="servicio in empresa.servicios" :key="servicio.id" >
+        <div v-if="servicio.tipo == 'traduccion'" class="row mb-12" >
+        <span class="col-2 bg-ligth border border-dark">{{
+          empresa.nombre
+        }}</span>
+        <span class="col-2 bg-ligth border border-dark">{{
+          servicio.idioma
+        }}</span>
+        <span class="col-3 bg-ligth border border-dark">{{
+          servicio.tipoDocumento
+        }}</span>
+        <span class="col-2 bg-ligth border border-dark">{{
+          servicio.tiempoEntrega
+        }}</span>
+        <span class="col-2 bg-ligth border border-dark">{{
+          servicio.traduccionJurada
+        }}</span>
+      </div>
     </div>
   </div>
+</div>
 </template>
+
 <style scoped>
 .container-fluid {
   width: 100%;
@@ -49,15 +60,28 @@ span {
   text-align: center;
 }
 
+.col-1 {
+  width: 10%;
+  text-align: center;
+}
+
 .col-3 {
   width: 25%;
   text-align: center;
 }
+
+.lapiz {
+  color: rgb(110, 60, 60);
+  padding-right: 0.5%;
+}
+.bin {
+  margin-right: 1%;
+}
 @media (max-width: 768px) {
   .container-fluid {
-    width: 100%;
-    margin-left: 2%;
-    font-size: 0.7rem;
-  }
+  width: 100%;
+  margin-left: 2%;
+  font-size: 0.7rem;
+}
 }
 </style>
