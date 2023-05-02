b5b1c543c8c9f7de104d3110a78654348d6c12fc
Csesvaz <camilocococo@gmail.com>
2023-05-02 10:13:24 +0200

    Creado componente Barranavegación

---
+++ frontend/src/components/BarraNavegacion.vue
@@ -0,0 +1,39 @@
+<script></script>
+
+<template>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary bg-primary">
+    <div class="container-fluid">
+      <div class="navbar-brand">Inicio</div>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <div class="nav-link" aria-current="page">
+              Servicios Disponibles
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+.navbar-brand:hover {
+  color: white;
+}
+
+.nav-link:hover {
+  color: white;
+}
+</style>
