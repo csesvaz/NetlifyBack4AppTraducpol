d5fbef4c21d5c85c13055e0f6fa728c79f6a6dcd
Csesvaz <camilocococo@gmail.com>
2023-05-22 07:04:00 +0200

    Formulario AltaEmpresa

--- frontend/src/components/BarraNavegacion.vue
+++ frontend/src/components/BarraNavegacion.vue
@@ -6,7 +6,7 @@
 <template>
 <nav class="navbar navbar-expand-lg bg-body-tertiary bg-primary">
   <div class="container-fluid">
-    <router-link class="navbar-brand" to="/">Inicio</router-link>
+    <router-link class="navbar-brand text-white" to="/">Inicio</router-link>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
       aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -14,19 +14,19 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <router-link class="nav-link" aria-current="page" to="servicio">Servicios Disponibles</router-link>
+          <router-link class="nav-link text-white" aria-current="page" to="servicio">Servicios Disponibles</router-link>
         </li>
         <li class="nav-item">
-          <router-link class="nav-link" to="/busqueda">Búsqueda</router-link>
-        </li>
-        <!-- <li class="nav-item">
-          <router-link class="nav-link" to="/alta">Gestión</router-link>
-        </li> -->
-        <li class="nav-item">
-          <router-link class="nav-link" to="/empresas">Empresas</router-link>
+          <router-link class="nav-link text-white" to="/busqueda">Búsqueda</router-link>
         </li>
         <li class="nav-item">
-          <router-link class="nav-link" to="/ayuda">Ayuda</router-link>
+          <router-link class="nav-link text-white" to="/alta">Gestión</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link text-white" to="/empresas">Empresas</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link text-white" to="/ayuda">Ayuda</router-link>
         </li>
       </ul>
      
@@ -39,12 +39,13 @@
 
 
 <style scoped>
-
 .navbar-brand:hover {
-  color: white;
+ 
+  background-color: rgb(9, 2, 105) !important;
 }
 
 .nav-link:hover {
-  color: white;
+
+  background-color: rgb(9, 2, 105) !important;
 }
 </style>
