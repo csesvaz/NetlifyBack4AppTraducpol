f6eb8f05639cc1a14c90d4971ecc584cb8e6e870
Csesvaz <camilocococo@gmail.com>
2023-06-22 20:03:37 +0200

    Añadido CORS y api-services

--- frontend/src/stores/api-service.js
+++ frontend/src/stores/api-service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const host = "https://traducpol-camilocococo.b4a.run/api"
+const host = "https://traducpol-camilocococo.b4a.run/api/"
 //const host = "http://localhost:8085/api/";
 
 export function cambiarHttpPorHttps(enlace) {
